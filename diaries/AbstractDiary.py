c5a617f2923c8adf9b445dd8d8964abc57d6a1d8
Minmin <kwm067t93@gmail.com>
2022-12-08 15:26:44 +0900

    First commit

---
+++ diaries/AbstractDiary.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractclassmethod
+
+class AbstractDiary(ABC):
+
+    def get_date(self):
+        pass
+
+    def get_summary(self):
+        pass
+    
+    def get_author(self):
+        pass
