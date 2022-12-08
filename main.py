5e8d81afd9e5046da9492b85ae71e840beca3ab8
福原優太 <fuyu0853@icloud.com>
2022-12-08 15:52:48 +0900

    福原の日記を新規追加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.FukuharaDiary import FukuharaDiary
 
 #↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(), 
+    FukuharaDiary(),
 ]
 
 for d in diaries:
