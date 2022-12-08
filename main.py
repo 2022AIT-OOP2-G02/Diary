c7c3610aa91d5e0f9c144d25c3c85f78b5633869
lmori-78 <k21025kk@aitech.ac.jp>
2022-12-08 15:52:53 +0900

    おおもり日記追加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.omoriDiary import omoriDiary
 
 #↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(), 
+    omoriDiary(),
 ]
 
 for d in diaries:
